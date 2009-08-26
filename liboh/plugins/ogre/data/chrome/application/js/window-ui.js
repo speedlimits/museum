887ed099017ef3c49893ac7f943f6dbec63a7806
Dennis S <dennis@dennisschaaf.com>
2009-08-25 23:22:15 -0700

    added web

---
+++ liboh/plugins/ogre/data/chrome/application/js/window-ui.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {	
+	
+	$("#mainContent").droppable({
+      	drop: function(event, ui) {
+		 	wizzard_setMessage("You dropped an object at" + event.pageX +
+							   ',' + event.pageY + "image id" + ui.helper[0].children[0].id + "<a href='javascript: wizzard_hideMessage();'>Ok</a>") ;
+			wizzard_showMessage();
+			alert();
+		}
+		
+	});
+	
+	$(".topbarbutton").bind('mouseover', function(event){
+												  
+		$(this).css('background-position-y', '-38px');						
+	});
+	$(".topbarbutton").bind('mouseout', function(event){
+		$(this).css('background-position-y', '0px');						
+	});
+	$(".topbarbutton").bind('mousedown', function(event){
+		$(this).css('background-position-y', '-76px');						
+	});
+	
+	
+	/* Navigation Stuff */
+	
+	$("#navi_up").bind('mousedown' , function() {
+		Client.event("navmoveforward", 1.0)
+	});
+	$("#navi_up").bind('mouseup' , function() {
+		Client.event("navmoveforward", 0.0)
+	});
+	$("#navi_up").bind('mouseout' , function() {
+		Client.event("navmoveforward", 0.0)
+	});
+	
+	$("#navi_down").bind('mousedown' , function() {
+		Client.event("navback", 1.0)
+	});
+	$("#navi_down").bind('mouseup' , function() {
+		Client.event("navback", 0.0)
+	});
+	$("#navi_down").bind('mouseout' , function() {
+		Client.event("navback", 0.0)
+	});
+
+	$("#navi_left").bind('mousedown' , function() {
+		Client.event("navturnleft", 1.0)
+	});
+	$("#navi_left").bind('mouseup' , function() {
+		Client.event("navturnleft", 0.0)
+	});
+	$("#navi_left").bind('mouseout' , function() {
+		Client.event("navturnleft", 0.0)
+	});
+
+	$("#navi_right").bind('mousedown' , function() {
+		Client.event("navturnright", 1.0)
+	});
+	$("#navi_right").bind('mouseup' , function() {
+		Client.event("navturnright", 0.0)
+	});
+	$("#navi_right").bind('mouseout' , function() {
+		Client.event("navturnright", 0.0)
+	});
+
+
+
+
+	
+	$("#navi_right").click( function() {
+		alert("Right") ;
+		wizzard_showMessage();
+	
+	});
+	
+	$(".ui").disableSelection();
+	
+});
