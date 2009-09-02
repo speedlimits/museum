b5c10483f5b5276e705d91a415dc54ef8d3c3dfb
danx0r <danbmil99@gmail.com>
2009-09-01 19:30:50 -0700

    kluge to not load UI in flythru mode if anyone has a better way, please go for it

--- liboh/plugins/ogre/WebViewManager.cpp
+++ liboh/plugins/ogre/WebViewManager.cpp
@@ -90,8 +90,12 @@ WebViewManager::WebViewManager(Ogre::Viewport* defaultViewport, InputManager* in
     //tooltipWebView->setIgnoresMouse();
 
         chromeWebView = createWebView("__chrome", 1024, 768, OverlayPosition(RP_TOPCENTER), false, 70, TIER_FRONT);
-        //chromeWebView->loadFile("application/sirikata.html");
-        chromeWebView->loadFile("application/sirikata_blank.html");
+        if (access("mode_flythru", F_OK)==0) {
+            chromeWebView->loadFile("application/sirikata_blank.html");
+        }
+        else {
+            chromeWebView->loadFile("application/sirikata.html");
+        }
         chromeWebView->setTransparent(true);
 
 /*  WebView* chromeUI = createWebView("ui", 1024, 768, OverlayPosition(RP_TOPCENTER), false, 70, TIER_MIDDLE);
