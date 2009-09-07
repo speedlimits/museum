25f76759644c54f67cf7cc57945443be42b7dbb3
unknown <Owner@.(none)>
2009-09-06 20:22:19 -0700

    added new chrome ui

---
+++ liboh/plugins/ogre/data/chrome/ui/js/sirikata.ui.js
@@ -0,0 +1,38 @@
+
+function stopPropagation(event, ui) { event.stopPropagation() ; }
+
+$(document).ready(function() {	
+	
+	$("#mainContent").droppable({
+      	drop: function(event, ui) {
+			debug("placing object");
+			debug("inventory placeObject " + ui.helper[0].children[0].id + " " + event.pageX + " " + event.pageY) ;
+			
+			Client.event("navcommand", "inventory placeObject " + ui.helper[0].children[0].id + " " + event.pageX + " " + event.pageY);
+			return false; //reject it so that position gets reverted
+
+		}
+		
+	} ) ;
+
+	$("#development-screen").draggable({ distance: 20, delay: 500 });
+/*	$("#navigation-screen").draggable();
+	$("#development-console-screen").draggable();
+
+
+	$("#development-console-screen p").bind('mousedown', stopPropagation ) ;
+	$("#development-console-screen p").bind('mouseup', stopPropagation ) ;
+*/
+
+	
+	
+	
+	
+	$(".ui").disableSelection();
+	
+});
+
+
+function navi_pic (pic) {
+	$("#navigation-picture").attr('src', "images/navigation/navigation_" + pic + ".png");
+}
