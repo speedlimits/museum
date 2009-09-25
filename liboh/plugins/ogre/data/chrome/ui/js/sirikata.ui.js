fd06b83c52545b89a5274cf65a56d36fbe79aead
danx0r <danbmil99@gmail.com>
2009-09-25 09:49:18 -0700

    dennis & patrick patches to critic

--- liboh/plugins/ogre/data/chrome/ui/js/sirikata.ui.js
+++ liboh/plugins/ogre/data/chrome/ui/js/sirikata.ui.js
@@ -2,12 +2,26 @@
 function stopPropagation(event, ui) { event.stopPropagation() ; }
 
 $(document).ready(function() {	
+
 	
 	$("#drop-screen").droppable({
       	drop: function(event, ui) {
+			var curatorbox=$("#detail-screen");
+			if (curatorbox) {
+				var offset = curatorbox.offset();
+				debug("Offset: "+offset.left+","+offset.top+"; event: "+event.pageX+","+event.pageY);
+				debug("Width: " + curatorbox.width() +", Height: "+curatorbox.height());
+				if ((event.pageX > offset.left
+					&& event.pageX < offset.left + curatorbox.width()) &&
+					(event.pageY > offset.top
+					&& event.pageY < offset.top + curatorbox.height())) {
+					debug("Dropped inside curator box! Ignoring...");
+					return;
+				}
+			}
 			debug("placing object");
-			debug("inventory placeObject " + ui.helper[0].children[0].id + " " + event.pageX + " " + event.pageY) ;
 			try {			  
+			  debug("inventory placeObject " + ui.helper[0].children[0].id + " " + event.pageX + " " + event.pageY) ;
 			  Client.event("navcommand", "inventory placeObject " + ui.helper[0].children[0].id + " " + event.pageX + " " + event.pageY);
 			} catch(err) {debug('error placing object');}
 			
